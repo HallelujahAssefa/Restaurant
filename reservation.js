01620e43cba9d441c94b4b23a8a611868e771c41
HallelujahAssefa <80492451+HallelujahAssefa@users.noreply.github.com>
2021-08-06 13:05:05 -0400

    Update reservation.js

--- reservation.js
+++ reservation.js
@@ -1,21 +1,6 @@
-const mq = window.matchMedia("(max-width: 586px)");
-
-if(mq.matches){
-const ul = document.querySelector('.navUL');
-const button = document.querySelector('.hButton');
 const menuBar = document.querySelector('.menuBar');
+const navUL = document.querySelector('.navUL');
 
-
-function showNav(){
-    if(ul.style.display != "block" & button.style.display !="block") {
-        ul.style.display = "block";
-        button.style.display ="block";
-    } else {
-        ul.style.display = "none";
-        button.style.display ="none";
-    }
-}
-
-
-menuBar.addEventListener('click', showNav)
-}
+menuBar.addEventListener('click', () =>{
+    navUL.classList.toggle('active')
+})
